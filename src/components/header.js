e1bb379258ebc080da42ad66ccf05fd87a787c36
KenzoBenzo <makennasmutz@gmail.com>
2018-10-27 16:21:45 +0000

    MT course 3

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,17 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './Header.css'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
+	<div className="Header">
+		<div className="HeaderGroup">
+			<Link to="/"><img src="https://cl.ly/58bce134a02c/download/GCMS%20logo.png" height="30px"/></Link>
+			<Link to="https://docs.graphcms.com/tutorials/">Tutorials</Link>
+			<Link to="https://github.com/KenzoBenzo/React-for-Designers-GraphCMS-upgrade">Source Code</Link>
+			<Link to="/page-2">Help</Link>
+			<Link to="https://app.graphcms.com/signup"><button>Try Now!</button></Link>
+		</div>
+	</div>
 )
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,7 @@ const IndexPage = () => (
 			<div className="HeroGroup">
 				<h1>Learn to <br />design and code React apps</h1>
 				<p>Complete courses about the best tools and design systems. Next up - <a href="https://graphcms.com/">GraphCMS</a>.</p>
-				<Link to="/page-2/">Go to page 2</Link>
-				<Link to="/video">Watch the video</Link>
+				<Link to="/video" className="videolink">Watch the video</Link>
 			</div>
 		</div>
 	</div>
